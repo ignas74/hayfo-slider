a5eee00529079e432c22f5e58e14b5103c2750dd
Ignas <ignas.lpnsks@gmail.com>
2023-11-03 08:54:10 +0200

    front end remake

--- assets/index.php
+++ assets/index.php
@@ -1,21 +1,12 @@
-<div class="hfo-container">
-    <button class="hfo-handle hfo-left-handle">
-        <div class="hfo-text">&#8249;</div>
-    </button>
-    
-    <div class="hfo-slider">
-        <?php foreach ( $products as $product ) : ?>
-            <div class="hfo-single-product">
-                <a href="<?php echo esc_url( $product->get_permalink() );?>">
-                    <?php echo $product->get_image();?>
-                </a>
-                <span><?php echo esc_html( $product->get_title() ); ?></span>
-                <span><?php echo esc_html( $product->get_price() ); ?></span>
-            </div>
-        <?php endforeach; ?>
-    </div>
 
-    <button class="hfo-handle hfo-right-handle">
-        <div class="hfo-text">&#8250;</div>
-    </button>
+<div class="hfo-slider">
+    <?php foreach ( $products as $product ) : ?>
+        <div class="hfo-single-product">
+            <a href="<?php echo esc_url( $product->get_permalink() );?>">
+                <?php echo $product->get_image();?>
+            </a>
+            <span><?php echo esc_html( $product->get_title() ); ?></span>
+            <span><?php echo esc_html( $product->get_price() ); ?></span>
+        </div>
+    <?php endforeach; ?>
 </div>
--- assets/slider-script.js
+++ assets/slider-script.js
@@ -1,50 +1,13 @@
-'use strict'
+console.log('check')
 
-// console.log('CHECKs: ' + Object.values(frames.value) )
+var $ = jQuery;
 
-// Set slider's frames from Admin panel to CSS
-document.addEventListener('DOMContentLoaded', () => {
-    const sliders = document.querySelector('.hfo-slider')
-    sliders.style.setProperty('--items-per-screen', Object.values(frames.value)) 
-})
-
-document.addEventListener('click', e => {
-    // Fix for .text usage inside the button .handle
-    let handle
-    
-    if (e.target.matches('.hfo-handle')) {
-        handle = e.target
-    } else {
-        handle = e.target.closest('.hfo-handle')
-    }
-    
-    if (handle != null) {
-        onHandleClick(handle)
-    }
-})
-
-function onHandleClick(handle) {
-    const slider = handle.closest('.hfo-container').querySelector('.hfo-slider')
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
-    const itemsPerScreen = parseInt(getComputedStyle(slider).getPropertyValue('--items-per-screen'))
-
-    // console.log(`Items per screen from function: ${itemsPerScreen}`)
-
-    const maxIndex = Math.ceil(slider.childElementCount / itemsPerScreen) - 1
-
-    let newIndex
-
-    if (handle.classList.contains('hfo-left-handle')) {
-        newIndex = sliderIndex - 1
-    } else if (handle.classList.contains('hfo-right-handle')) {
-        newIndex = sliderIndex + 1
-    }
-
-    if (newIndex < 0) {
-        newIndex = maxIndex
-    } else if (newIndex > maxIndex) {
-        newIndex = 0
-    }
-
-    slider.style.setProperty('--slider-index', newIndex)
-}
+$('.hfo-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    // infinite: true,
+    arrows: true,
+    // dots: true,
+    // appendArrows: '',
+    // dotsPosition: 'bottom',
+  });
